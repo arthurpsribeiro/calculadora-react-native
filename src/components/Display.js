9a562d539383765f57d21bfe7f59727cb5dc9f43
arthurpsribeiro <arthurpsribeiro@gmail.com>
2021-06-23 09:31:49 -0300

    display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,7 +2,22 @@ import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 
 export default (props) => {
-	return <></>;
+	return (
+		<View style={styles.display}>
+			<Text style={styles.displayValue}>0</Text>
+		</View>
+	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	display: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "flex-end",
+		backgroundColor: "rgba(0, 0, 0, 0.6)",
+	},
+	displayValue: {
+		fontSize: 60,
+		color: "#fff",
+	},
+});
--- src/screens/Calculator.js
+++ src/screens/Calculator.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import { Text, StyleSheet, View } from "react-native";
+import Display from "../components/Display";
 
 export default class Calculator extends Component {
 	state = {};
 
 	render() {
-		return <></>;
+		return (
+			<View>
+				<Display></Display>
+			</View>
+		);
 	}
 }
 
