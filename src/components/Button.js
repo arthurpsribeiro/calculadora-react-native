a514f35aee0aa781b6ad7bc5541c4829684ef8ef
arthurpsribeiro <arthurpsribeiro@gmail.com>
2021-06-23 10:35:11 -0300

    button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,39 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, TouchableOpacity, StyleSheet, Dimensions } from "react-native";
 
 export default (props) => {
-	return;
+	const stylesButton = [styles.main];
+
+	if (props.operation) stylesButton.push(styles.operation);
+	if (props.twoCollumns) stylesButton.push(styles.twoCollumns);
+	if (props.threeCollumns) stylesButton.push(styles.threeCollumns);
+
+	return (
+		<TouchableOpacity>
+			<Text style={[stylesButton]}>{props.label}</Text>
+		</TouchableOpacity>
+	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	main: {
+		fontSize: 40,
+		height: Dimensions.get("window").width / 4,
+		width: Dimensions.get("window").width / 4,
+		backgroundColor: "#f0f0f0",
+		textAlign: "center",
+		padding: 20,
+		borderWidth: 1,
+		borderColor: "#999",
+	},
+	operation: {
+		color: "#fff",
+		backgroundColor: "#fa8231",
+	},
+	twoCollumns: {
+		width: (Dimensions.get("window").width / 4) * 2,
+	},
+	threeCollumns: {
+		width: (Dimensions.get("window").width / 4) * 3,
+	},
+});
--- src/screens/Calculator.js
+++ src/screens/Calculator.js
@@ -1,17 +1,45 @@
 import React, { Component } from "react";
 import { Text, StyleSheet, View } from "react-native";
 import Display from "../components/Display";
+import Button from "../components/Button";
 
 export default class Calculator extends Component {
 	state = {};
 
 	render() {
 		return (
-			<View>
-				<Display></Display>
+			<View style={styles.container}>
+				<Display />
+				<View style={styles.buttons}>
+					<Button label="AC" threeCollumns />
+					<Button label="/" operation />
+					<Button label="7" />
+					<Button label="8" />
+					<Button label="9" />
+					<Button label="*" operation />
+					<Button label="4" />
+					<Button label="5" />
+					<Button label="6" />
+					<Button label="-" operation />
+					<Button label="1" />
+					<Button label="2" />
+					<Button label="3" />
+					<Button label="+" operation />
+					<Button label="0" twoCollumns />
+					<Button label="." />
+					<Button label="=" operation />
+				</View>
 			</View>
 		);
 	}
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	buttons: {
+		flexDirection: "row",
+		flexWrap: "wrap",
+	},
+});
